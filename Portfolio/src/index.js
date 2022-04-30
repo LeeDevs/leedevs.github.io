510062f92b5ea241803f00c1ca1b5505b07b16d4
LeeDevs <yaidan120@gmail.com>
2022-04-30 15:17:37 -0400

    Huge Update to css js and html

--- Portfolio/src/index.js
+++ Portfolio/src/index.js
@@ -1,5 +1,27 @@
-let menu = document.querySelector('.menu-icon');
+$("#toggle").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
 
-menu.onclick = () => {
-    menu.classList.toggle('move')
-}
+$("#resize ul li a").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+$(".close-btn").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+TweenMax.from("#brand", 1, {
+    delay: 0.4,
+    y: 10,
+    opacity: 0,
+    ease: expo.easeInOut
+});
+
+TweenMax.staggerFrom("#menu li a", 1, {
+    delay: 0.4,
+    opacity: 0,
+    ease: expo.easeInOut
+}, 0.1);
